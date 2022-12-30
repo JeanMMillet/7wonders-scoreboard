42a3b06994bd27bd614dcafc660f34f46c7defa3
Jean Marc <millet.jm@live.com>
2022-12-30 21:40:07 +0100

    reset folder

--- script.js
+++
@@ -1,7 +0,0 @@
-const message = "Thank you for submitting";
-
-document.getElementById("contactForm");
-document.addEventListener("submit", function (event) {
-  event.preventDefault();
-  alert(message);
-});
