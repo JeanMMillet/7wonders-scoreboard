91592e4c76f98e1bfa5a5884c83b4020160af574
JeanMMillet <millet.jm@live.com>
2023-04-21 11:42:20 +0200

    Add science window on click

--- src/PlayerRow.tsx
+++ src/PlayerRow.tsx
@@ -1,12 +1,12 @@
 import { useState,useEffect } from "react";
+import ScienceWindow from "./ScienceWindow";
 
 export default function PlayerRow({player, players, index, setPlayers}) {
   
   const [playerName, setPlayerName] = useState("");
   const [militaryPoints, setMilitaryPoints] = useState(0);
   const [coinPoints, setCoinPoints] = useState(0);
-  const [wonderPoints,setWonderPoints] = useState(0) 
-  
+  const [wonderPoints,setWonderPoints] = useState(0);
   const [culturePoints, setCulturePoints] = useState(0);
   const [tradingPoints, setTradingPoints] = useState(0);
   const [sciencePoints, setSciencePoints] = useState(0);
@@ -17,24 +17,26 @@ export default function PlayerRow({player, players, index, setPlayers}) {
     setVisible(true);
   }
   useEffect(() => {
+    // Update the table of players after the change of any points
     let updatePlayers = players.map((e:object, i:number) => {
       if (i === index) {
         return {
           ...player,
           playerName: playerName,
-          militaryPoints: +militaryPoints,
-          coinPoints: +coinPoints,
-          culturePoints: +culturePoints,
-          tradingPoints: +tradingPoints,
+          militaryPoints: militaryPoints,
+          coinPoints: coinPoints,
+          wonderPoints: wonderPoints,
+          culturePoints: culturePoints,
+          tradingPoints: tradingPoints,
           score:
-            militaryPoints + coinPoints + culturePoints + tradingPoints,
+            militaryPoints + coinPoints + culturePoints + tradingPoints + wonderPoints,
         };
       } else {
         return e;
       }
     });
     setPlayers(updatePlayers);
-  }, [militaryPoints, coinPoints, culturePoints, tradingPoints, playerName]);
+  }, [militaryPoints, coinPoints, culturePoints, tradingPoints, playerName, wonderPoints]);
 
   return (
     <div>
@@ -88,8 +90,15 @@ export default function PlayerRow({player, players, index, setPlayers}) {
             min={0}
             value={sciencePoints}
           />
-       
-          <input type="number" className="score" readOnly={true} value={tradingPoints+culturePoints+coinPoints+militaryPoints+sciencePoints} />
+
+          <input 
+          type="number" 
+          className="score" 
+          readOnly={true} 
+          value={tradingPoints+culturePoints+wonderPoints+coinPoints+militaryPoints+sciencePoints} 
+          />
+          
+          {visible? <ScienceWindow player={player} setVisible={setVisible} setSciencePoints={setSciencePoints} /> : ""}
     </div>
   )
          
