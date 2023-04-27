73a18bcd3c9cc933752608f1cc3f2af6de11163c
JeanMMillet <millet.jm@live.com>
2023-04-27 11:24:37 +0200

    calcul in ts file

---
+++ src/ScienceScoreCalculator.ts
@@ -0,0 +1,20 @@
+
+
+export default function scienceScoreCalculation (tablet:number, wheel:number, compass:number, bonus:number) {
+      
+      let scienceCards = [tablet, wheel, compass];
+      scienceCards = scienceCards.sort()
+      if (bonus ===1)  {
+            if (scienceCards[0] == scienceCards[1] || scienceCards[2] ===4) {
+                  scienceCards[2] += 1
+            }
+            else {
+                  scienceCards[0] += 1
+            }
+      }
+
+      let bonusScore = 7*scienceCards[0];
+
+      return scienceCards[0] **2 +scienceCards[1] **2+scienceCards[2] **2 + bonusScore
+
+}
